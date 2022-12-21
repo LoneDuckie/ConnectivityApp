dfcd63b6c5bbe2d873b7ca70640bbb71c1fc5be2
LoneDuckie <raddim.novak@gmail.com>
2022-12-21 11:37:19 +0100

    Add project files.

---
+++ ConnectivityAppWPF/Devices.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConnectivityAppWPF
+{
+    internal class Devices
+    {
+        public string DeviceLocation { get; set; }
+        public string Station { get; set; }
+        public string DeviceType { get; set; }
+        public string DeviceID { get; set; }
+        public string ControlUnit { get; set; }
+        public string PLCrevision { get; set; }
+        public string IoTport { get; set; }
+        public string IPadress { get; set; }
+        public string MacAdress { get; set; }
+        public string LastBackUp { get; set; }
+        public string Link { get; set; }
+        public string Comment { get; set; }
+
+        public Devices(string deviceLocation, string station, string deviceType, string deviceID, string controlUnit, string pLCrevision, string ioTport, string iPadress, string macAdress, string lastBackUp, string link, string comment)
+        {
+            DeviceLocation = deviceLocation;
+            Station = station;
+            DeviceType = deviceType;
+            DeviceID = deviceID;
+            ControlUnit = controlUnit;
+            PLCrevision = pLCrevision;
+            IoTport = ioTport;
+            IPadress = iPadress;
+            MacAdress = macAdress;
+            LastBackUp = lastBackUp;
+            Link = link;
+            Comment = comment;
+        }
+    }
+}
---
+++ ConnectivityAppWPF/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ConnectivityAppWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        List<string> lines = new List<string>();
+        List<Devices> devices = new List<Devices>();
+
+        private void CloseBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void OpenFile_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                OpenFileDialog openFileDialog = new OpenFileDialog();
+                if (openFileDialog.ShowDialog() == true)
+                    using (var sr = new StreamReader(openFileDialog.FileName))
+                    {
+                        string line;
+                        while ((line = sr.ReadLine()) != null) 
+                        {
+                            lines.Add(line);
+                        }
+                    }
+
+                foreach (var item in lines)
+                {
+                    string[] arr = item.Split(';');
+                    if (arr.Length > 11)
+                    { 
+                            Devices device = new Devices(arr[0], arr[1], arr[2], arr[3], arr[4], arr[5], arr[6], arr[7], arr[8], arr[9], arr[10], arr[11]);
+                            devices.Add(device);
+
+                    }
+                }
+
+                VypisLV.ItemsSource = devices;
+            }
+            catch (Exception ex) 
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
